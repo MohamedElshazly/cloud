c14978effba4594a0f6ed42c21001ab298b4c26f
Mohamed Elshazly <22671838+MohamedElshazly@users.noreply.github.com>
2023-02-18 18:32:55 +0200

    add text effect

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const blob = document.getElementsByClassName('blob')[0];
+const h1 = document.getElementsByTagName('h1')[0];
+const p = document.getElementsByTagName('p')[0];
 
-
-//make blob follow mouse movement
 document.addEventListener('mousemove', (e) => {
 	const {clientX, clientY} = e;
 		
@@ -11,3 +11,37 @@ document.addEventListener('mousemove', (e) => {
 	}, {duration: 3000, fill: 'forwards'});
 }
 );
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval1 = null; 
+h1.onmouseover = e => {
+	let iterations = 0;
+	clearInterval(interval1);
+
+	interval1 = setInterval(() => {
+		h1.innerText = h1.innerText.split('').map((l, i) => i < iterations ? e.target.dataset.value[i] : letters[Math.floor(Math.random() * letters.length)]).join('');
+		
+		if(iterations >= e.target.dataset.value.length) {
+			clearInterval(interval1);
+		}
+		iterations += 1 / 3;
+	}
+, 30);
+}
+
+let interval2 = null; 
+p.onmouseover = e => {
+	let iterations = 0;
+	clearInterval(interval2);
+
+	interval2 = setInterval(() => {
+		p.innerText = p.innerText.split('').map((l, i) => i < iterations ? e.target.dataset.value[i] : letters[Math.floor(Math.random() * letters.length)]).join('');
+		
+		if(iterations >= e.target.dataset.value.length) {
+			clearInterval(interval2);
+		}
+		iterations += 1 / 3;
+	}
+, 30);
+}
